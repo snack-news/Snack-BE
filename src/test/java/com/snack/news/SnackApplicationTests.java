9d36f646ed617cfdc080753c6770e8e7671eb00e
Kwon Young Jae <kyoje11@gmail.com>
2019-02-12 21:41:30 +0900

    🍺 Init Codecover

--- src/test/java/com/snack/news/SnackApplicationTests.java
+++ src/test/java/com/snack/news/SnackApplicationTests.java
@@ -5,13 +5,16 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SnackApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void test() {
+		String test = "Nesoy";
+		assertThat(test).isEqualTo("Nesoy");
 	}
-
 }
 
