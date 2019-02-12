121aa33ab2379ace0d3e1a3c00cb1e182fa57f7e
Kwon Young Jae <kyoje11@gmail.com>
2019-02-12 20:52:30 +0900

    🍺 Init Snack News

---
+++ README.md
@@ -0,0 +1,1 @@
+## Snack News - BE
---
+++ src/main/java/com/snack/news/SnackApplication.java
@@ -0,0 +1,14 @@
+package com.snack.news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackApplication.class, args);
+	}
+
+}
+
