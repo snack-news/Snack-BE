a59f77e40443668fb0384023d00dbf4ae26a1eb2
Kwon Young Jae <kyoje11@gmail.com>
2019-04-11 21:11:32 +0900

    Refactoring Exception

--- src/main/java/com/snack/news/exception/CorpNotFoundException.java
+++ src/main/java/com/snack/news/exception/CorpNotFoundException.java
@@ -2,5 +2,5 @@ package com.snack.news.exception;
 
 import com.snack.news.exception.base.NotFoundException;
 
-public class NewsNotFoundException extends NotFoundException {
+public class CorpNotFoundException extends NotFoundException {
 }
---
+++ src/main/java/com/snack/news/exception/base/NotFoundException.java
@@ -0,0 +1,8 @@
+package com.snack.news.exception.base;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public abstract class NotFoundException extends RuntimeException {
+}
