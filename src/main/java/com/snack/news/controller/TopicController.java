21593021a1daec7f6d84f24e77acdc099caf0a89
Delf-Lee <ttjs2008@naver.com>
2019-07-17 23:57:47 +0900

    Topic 컨트롤러 반환값 ResponseEntity로 변경

--- src/main/java/com/snack/news/controller/TopicController.java
+++ src/main/java/com/snack/news/controller/TopicController.java
@@ -3,12 +3,16 @@ package com.snack.news.controller;
 import com.snack.news.domain.Topic;
 import com.snack.news.domain.TopicType;
 import com.snack.news.dto.TopicDto;
+import com.snack.news.exception.TopicNotFoundException;
 import com.snack.news.service.TopicService;
 import com.snack.news.strategy.TopicSorting;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
@@ -18,18 +22,22 @@ public class TopicController {
 
 	// todo : Domain이 직접 노출되는 문제
 	@PostMapping
-	public Topic createTopic(@RequestBody TopicDto topicDto) {
-		return topicService.createTopic(topicDto);
+	public ResponseEntity<Topic> createTopic(@RequestBody TopicDto topicDto) {
+		return ResponseEntity.of(Optional.of(topicService.createTopic(topicDto)));
 	}
 
 	@GetMapping
-	public List<Topic> getTopicList(@RequestParam(defaultValue = "NAME") TopicSorting sort) {
-		return topicService.getTopicList(sort);
+	public ResponseEntity<List<Topic>> getTopicList(@RequestParam(defaultValue = "NAME") TopicSorting sort) {
+		return ResponseEntity.of(Optional.of(topicService.getTopicList(sort)));
 	}
 
 	@GetMapping("/{type}")
-	public List<Topic> getTopicList(@PathVariable TopicType type, @RequestParam(defaultValue = "NAME") TopicSorting sort) {
-		return topicService.getTypeTopicList(type, sort);
+	public ResponseEntity<List<Topic>> getTopicList(@PathVariable TopicType type, @RequestParam(defaultValue = "NAME") TopicSorting sort) {
+		try {
+			return ResponseEntity.of(Optional.of(topicService.getTypeTopicList(type, sort)));
+		} catch (IllegalArgumentException e) {
+			return ResponseEntity.notFound().varyBy("message", e.getMessage()).build();
+		}
 	}
 
 	@PutMapping
