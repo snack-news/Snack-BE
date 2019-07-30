e38037ac2ce07611bfffafe2e4e2d1049c550639
Delf-Lee <ttjs2008@naver.com>
2019-07-30 18:10:01 +0900

    WeekUtil 테스트 코드 추가

--- src/main/java/com/snack/news/util/WeekUtil.java
+++ src/main/java/com/snack/news/util/WeekUtil.java
@@ -4,11 +4,11 @@ import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 
 public class WeekUtil {
-	public static LocalDateTime getFirstDayWeekOf(LocalDateTime date) {
-		return date.minusDays(date.getDayOfWeek().minus(1).getValue()).withHour(0).withMinute(0).withSecond(0);
+	public static LocalDateTime getFirstDayOfWeek(LocalDateTime date) {
+		return date.minusDays(date.getDayOfWeek().getValue() - DayOfWeek.MONDAY.getValue()).withHour(0).withMinute(0).withSecond(0);
 	}
 
-	public static LocalDateTime getLastDayWeekOf(LocalDateTime date) {
+	public static LocalDateTime getLastDayOfWeek(LocalDateTime date) {
 		return date.plusDays(DayOfWeek.SUNDAY.getValue() - date.getDayOfWeek().getValue()).withHour(11).withMinute(59).withSecond(59);
 	}
 }
