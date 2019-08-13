f772ef78d90d03fdc5794513fc0ea649ef4e77c5
Delf-Lee <ttjs2008@naver.com>
2019-08-13 22:14:03 +0900

    [#66] News 조회시url parameter로 받을 수 있도록 변경

--- src/main/java/com/snack/news/controller/NewsController.java
+++ src/main/java/com/snack/news/controller/NewsController.java
@@ -5,12 +5,10 @@ import com.snack.news.dto.NewsDto;
 import com.snack.news.dto.WrappedResponse;
 import com.snack.news.service.NewsService;
 import lombok.AllArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @AllArgsConstructor
@@ -24,8 +22,8 @@ public class NewsController {
 		return WrappedResponse.ok(newsService.createNews(newsDto));
 	}
 
-	@GetMapping("/body")
-	public ResponseEntity<List<News>> getNewsListRequestBody(@RequestBody NewsDto newsDto) {
+	@GetMapping
+	public ResponseEntity<List<News>> getNewsListRequestBody(@ModelAttribute NewsDto newsDto) {
 		List<News> result = newsService.getNewsList(newsDto);
 		if (result.isEmpty()) {
 			return ResponseEntity.noContent().build();
@@ -33,30 +31,8 @@ public class NewsController {
 		return WrappedResponse.ok(result);
 	}
 
-
-	@GetMapping
-	public ResponseEntity<List<News>> getNewsListPathVariable(@RequestParam(required = false)
-															  @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime startDateTime,
-															  @RequestParam(required = false)
-															  @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm") LocalDateTime endDateTime,
-															  @RequestParam(required = false) Long categoryId,
-															  @RequestParam(required = false) List<Long> topicIds,
-															  @RequestParam(required = false) List<Long> tagIds) {
-
-		NewsDto requestNewsDto = NewsDto.builder()
-				.startDateTime(startDateTime)
-				.endDateTime(endDateTime)
-				.categoryId(categoryId)
-				.topicIds(topicIds)
-				.tagIds(tagIds)
-				.build();
-
-		return getNewsListRequestBody(requestNewsDto);
-	}
-
 	@GetMapping("/{newsId}")
 	public ResponseEntity<News> getNews(@PathVariable Long newsId) {
 		return WrappedResponse.ok(newsService.getNews(newsId));
 	}
 }
-
