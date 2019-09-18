4197f05cb50ddc53517964b2d97c4b54338833a9
Delf-Lee <ttjs2008@naver.com>
2019-09-19 00:47:18 +0900

    [#88] AdminController 분리

---
+++ src/main/java/com/snack/news/controller/AdminController.java
@@ -0,0 +1,36 @@
+package com.snack.news.controller;
+
+import com.snack.news.domain.news.News;
+import com.snack.news.dto.NewsDto;
+import com.snack.news.dto.WrappedResponse;
+import com.snack.news.service.NewsService;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/admin")
+public class AdminController {
+
+	private final NewsService newsService;
+
+	@GetMapping({"/news", "/news/{page}"})
+	public ResponseEntity<Page<News>> getNewsList(@SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+												  @PathVariable Optional<Integer> page) {
+		Page<News> result = newsService.getNewsListForAdmin(page.orElse(1));
+		if (result.isEmpty()) {
+			return ResponseEntity.noContent().build();
+		}
+		return WrappedResponse.ok(result);
+	}
+
+	@PostMapping("/news")
+	public ResponseEntity<NewsDto> createNews(@Valid @RequestBody NewsDto newsDto) {
+		return WrappedResponse.ok(newsService.createNews(newsDto));
+	}
+}
--- src/test/java/com/snack/news/controller/NewsControllerTest.java
+++ src/test/java/com/snack/news/controller/NewsControllerTest.java
@@ -5,7 +5,6 @@ import com.snack.news.dto.NewsDto;
 import com.snack.news.exception.NewsNotFoundException;
 import com.snack.news.fixture.NewsFixture;
 import com.snack.news.service.NewsService;
-import com.snack.news.util.SnackObjectMapper;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -22,7 +21,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -43,64 +41,7 @@ public class NewsControllerTest extends NewsFixture {
 		mockMvc = MockMvcBuilders.standaloneSetup(newsController).build();
 	}
 
-	@Test
-	public void 뉴스_생성_요청이_정상적으로_이루어진다() throws Exception {
-		NewsDto incorrectRequestNewsDtoForCreateNews = NewsDto.builder()
-				.title(TEST_TITLE)
-				.content(TEST_CONTENT)
-				.categoryId(TEST_SOME_ID_LONG)
-				.build();
 
-		String requestJsonBody = SnackObjectMapper.mapper.writeValueAsString(incorrectRequestNewsDtoForCreateNews);
-
-		when(newsService.createNews(any(NewsDto.class))).thenReturn(mockNewsDto);
-
-		mockMvc.perform(post(NEWS_API_URL)
-				.contentType(MediaType.APPLICATION_JSON).content(requestJsonBody))
-				.andExpect(status().isOk());
-	}
-
-	@Test
-	public void 뉴스_생성_요청시_제목을_입력하지_않으면_BADREQUEST_상태코드로_응답한다() throws Exception {
-		NewsDto incorrectRequestNewsDtoForCreateNews = NewsDto.builder()
-				.content(TEST_CONTENT)
-				.categoryId(TEST_SOME_ID_LONG)
-				.build();
-
-		String requestJsonBody = SnackObjectMapper.mapper.writeValueAsString(incorrectRequestNewsDtoForCreateNews);
-
-		mockMvc.perform(post(NEWS_API_URL)
-				.contentType(MediaType.APPLICATION_JSON).content(requestJsonBody))
-				.andExpect(status().isBadRequest());
-	}
-
-	@Test
-	public void 뉴스_생성_요청시_내용을_입력하지_않으면_BADREQUEST_상태코드로_응답한다() throws Exception {
-		NewsDto incorrectRequestNewsDtoForCreateNews = NewsDto.builder()
-				.title(TEST_TITLE)
-				.categoryId(TEST_SOME_ID_LONG)
-				.build();
-
-		String requestJsonBody = SnackObjectMapper.mapper.writeValueAsString(incorrectRequestNewsDtoForCreateNews);
-
-		mockMvc.perform(post(NEWS_API_URL)
-				.contentType(MediaType.APPLICATION_JSON).content(requestJsonBody))
-				.andExpect(status().isBadRequest());
-	}
-
-	@Test
-	public void 뉴스_생성_요청시_카테고리ID를_입력하지_않으면_BADREQUEST_상태코드로_응답한다() throws Exception {
-		NewsDto incorrectRequestNewsDtoForCreateNews = NewsDto.builder()
-				.title(TEST_TITLE)
-				.content(TEST_CONTENT)
-				.build();
-
-		String requestJsonBody = SnackObjectMapper.mapper.writeValueAsString(incorrectRequestNewsDtoForCreateNews);
-
-		mockMvc.perform(post(NEWS_API_URL)
-				.contentType(MediaType.APPLICATION_JSON).content(requestJsonBody))
-				.andExpect(status().isBadRequest());
-	}
 
 	@Test
 	public void 뉴스_조회_요청이_정상적으로_이루어진다() throws Exception {
