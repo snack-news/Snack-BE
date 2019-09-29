140f3ae7a2bea51c80f68b87063c1d1facf68ad7
Delf-Lee <ttjs2008@naver.com>
2019-09-29 23:18:41 +0900

    리펙토링: Period 테스트 코드 변수명 수정

--- src/test/java/com/snack/news/dto/PeriodTest.java
+++ src/test/java/com/snack/news/dto/PeriodTest.java
@@ -10,42 +10,42 @@ public class PeriodTest {
 
 	@Test
 	public void 두_날짜가_한주_내에_포함되고_같은_달이고_월요일로_시작한다() {
-		LocalDateTime start = LocalDateTime.of(2019, 7, 29, 0, 0); // 월
-		LocalDateTime end = LocalDateTime.of(2019, 7, 31, 0, 0); // 수
+		LocalDateTime startOfMonday = LocalDateTime.of(2019, 7, 29, 0, 0);
+		LocalDateTime endOfWednesday = LocalDateTime.of(2019, 7, 31, 0, 0);
 
-		new Period(start, end).validationCheck();
+		new Period(startOfMonday, endOfWednesday).validationCheck();
 	}
 
 	@Test
 	public void 두_날짜가_한주_내에_포함되고_같은_달이고_일요일로_끝난다() {
-		LocalDateTime start = LocalDateTime.of(2019, 8, 1, 0, 0); // 수
-		LocalDateTime end = LocalDateTime.of(2019, 8, 4, 0, 0); //일
+		LocalDateTime startOfWednesday = LocalDateTime.of(2019, 8, 1, 0, 0);
+		LocalDateTime endOfSunday = LocalDateTime.of(2019, 8, 4, 0, 0);
 
-		new Period(start, end).validationCheck();
+		new Period(startOfWednesday, endOfSunday).validationCheck();
 	}
 
 	@Test(expected = NewsBadRequestException.class)
 	public void 두_날짜가_한주_내에_포함되고_같은_달이지만_월요일로_시작하지_않고_일요일로_끝나지_않으면_예외가_발생한다() {
-		LocalDateTime start = LocalDateTime.of(2019, 8, 1, 0, 0); // 수
-		LocalDateTime end = LocalDateTime.of(2019, 8, 3, 0, 0); //토
+		LocalDateTime start = LocalDateTime.of(2019, 8, 1, 0, 0);
+		LocalDateTime endOfSaturday = LocalDateTime.of(2019, 8, 3, 0, 0);
 
-		new Period(start, end).validationCheck();
+		new Period(start, endOfSaturday).validationCheck();
 	}
 
 	@Test(expected = NewsBadRequestException.class)
 	public void 두_날짜가_한주_내에_포함되지만_달이_다르면_예외가_발생한다() {
-		LocalDateTime start = LocalDateTime.of(2019, 7, 29, 0, 0); // 월
-		LocalDateTime end = LocalDateTime.of(2019, 8, 4, 0, 0); //일
+		LocalDateTime startOfMonday = LocalDateTime.of(2019, 7, 29, 0, 0);
+		LocalDateTime endOfSunday = LocalDateTime.of(2019, 8, 4, 0, 0);
 
-		new Period(start, end).validationCheck();
+		new Period(startOfMonday, endOfSunday).validationCheck();
 	}
 
 	@Test(expected = NewsBadRequestException.class)
 	public void 두_날짜의_차가_일주일이_넘으면_예외가_발생한다() {
-		LocalDateTime start = LocalDateTime.of(2019, 7, 29, 0, 0); // 월
-		LocalDateTime end = LocalDateTime.of(2019, 8, 5, 0, 0); //월
+		LocalDateTime startOfMonday = LocalDateTime.of(2019, 7, 29, 0, 0);
+		LocalDateTime startOfNextMonday = LocalDateTime.of(2019, 8, 5, 0, 0);
 
-		new Period(start, end).validationCheck();
+		new Period(startOfMonday, startOfNextMonday).validationCheck();
 	}
 
 	@Test(expected = NewsBadRequestException.class)
