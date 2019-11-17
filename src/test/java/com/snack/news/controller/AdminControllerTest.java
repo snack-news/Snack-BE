658cfc962e43d9bb842da87658ffd203c57b3fe5
Delf-Lee <ttjs2008@naver.com>
2019-11-17 12:21:37 +0900

    import문 수정

--- src/test/java/com/snack/news/controller/AdminControllerTest.java
+++ src/test/java/com/snack/news/controller/AdminControllerTest.java
@@ -31,7 +31,9 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(MockitoJUnitRunner.class)
