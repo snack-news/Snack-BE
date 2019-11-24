8b85f3debfa458229e1ff064de3127dc1e5661c4
Delf-Lee <ttjs2008@naver.com>
2019-11-24 11:34:30 +0900

    import문 세부화

--- src/test/java/com/snack/news/service/AdminServiceTest.java
+++ src/test/java/com/snack/news/service/AdminServiceTest.java
@@ -19,7 +19,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.doThrow;
