25197232488b400d92a389690662e49086ad4e94
Delf-Lee <ttjs2008@naver.com>
2020-01-05 13:42:16 +0900

    [#149] 생성일을 지정할 수 있도록 변경

--- src/main/java/com/snack/news/domain/base/BaseTimeEntity.java
+++ src/main/java/com/snack/news/domain/base/BaseTimeEntity.java
@@ -1,20 +1,23 @@
 package com.snack.news.domain.base;
 
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Getter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseTimeEntity {
-	@CreatedDate
 	private LocalDateTime createAt;
 	@LastModifiedDate
 	private LocalDateTime modifiedAt;
+
+	protected void setCreateAt(LocalDateTime createAt) {
+		this.createAt = Optional.ofNullable(createAt).orElse(LocalDateTime.now());
+	}
 }
