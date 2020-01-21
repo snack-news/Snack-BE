bf8e3923d1460b0692de7ffb3363903bad3df746
Delf-Lee <ttjs2008@naver.com>
2020-01-21 23:18:55 +0900

    [#158] ServiceTest에서 날짜 검증 삭제

--- src/test/java/com/snack/news/service/NewsServiceTest.java
+++ src/test/java/com/snack/news/service/NewsServiceTest.java
@@ -1,7 +1,6 @@
 package com.snack.news.service;
 
 import com.snack.news.dto.NewsDto;
-import com.snack.news.exception.NewsBadRequestException;
 import com.snack.news.exception.NewsNotFoundException;
 import com.snack.news.fixture.NewsFixture;
 import com.snack.news.repository.NewsRepository;
@@ -12,10 +11,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -35,37 +33,6 @@ class NewsServiceTest extends NewsFixture {
 
 		newsService.getNewsList(newsDtoWithValidDates);
 		verify(newsRepository, times(2)).findByNewsDto(any(NewsDto.class));
-	}
-
-	@Test
-	@DisplayName("뉴스 리스트를 조회할 때 날짜가 없다면 예외가 발생한다")
-	void getNewsListTestWhenNoneDate() {
-		NewsDto newsDtoWithNoDates = NewsDto.builder()
-				.title(TEST_TITLE)
-				.content(TEST_CONTENT)
-				.build();
-
-		assertThrows(NewsBadRequestException.class, () -> newsService.getNewsList(newsDtoWithNoDates));
-	}
-
-	@Test
-	@DisplayName("뉴스 리스트를 조회할 때 날짜가 부적절하다면 예외가 발생한다")
-	void getNewsListTestWhenInvalidDate() {
-		NewsDto newsDtoWithInvalidDates = NewsDto.builder()
-				.title(TEST_TITLE)
-				.content(TEST_CONTENT)
-				.startDateTime(INVALID_START_DATE)
-				.endDateTime(INVALID_END_DATE)
-				.build();
-
-		assertThrows(NewsBadRequestException.class, () -> newsService.getNewsList(newsDtoWithInvalidDates));
-	}
-
-	@Test
-	@DisplayName("뉴스를 조회할 수 있다")
-	void getNewsTest() {
-		when(newsRepository.findById(anyLong())).thenReturn(Optional.of(mockNews));
-		newsService.getNews(TEST_SOME_ID_LONG);
 	}
 
 	@Test
