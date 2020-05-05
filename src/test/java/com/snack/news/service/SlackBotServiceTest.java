d4bb1f75109fdebfeff29a9c020c90ed583f19ff
Delf-Lee <ttjs2008@naver.com>
2020-05-05 17:18:40 +0900

    import 최적화

--- src/test/java/com/snack/news/service/SlackBotServiceTest.java
+++ src/test/java/com/snack/news/service/SlackBotServiceTest.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 
 @ExtendWith(MockitoExtension.class)
 class SlackBotServiceTest {
